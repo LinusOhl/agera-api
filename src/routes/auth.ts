5e4c7301764946bd80faf353e5357a6819c20c62
Linuss <tade3d@gmail.com>
2026-01-07 08:42:09 +0100

    Created the login route.

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
-import { createUser } from "../services/user.service.js";
+import { setCookie } from "hono/cookie";
+import { createUser, loginUser } from "../services/user.service.js";
 
 const app = new Hono();
 
@@ -11,6 +12,22 @@ app.post("/signup", async (c) => {
   return c.json({
     status: "success",
     data: result,
+  });
+});
+
+app.post("/login", async (c) => {
+  const body = await c.req.json();
+
+  const result = await loginUser(body);
+
+  setCookie(c, "refresh_token", result.refreshToken.token, {
+    httpOnly: true,
+    expires: result.refreshToken.expiresAt,
+  });
+
+  return c.json({
+    status: "success",
+    data: result.jwt,
   });
 });
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,18 @@
+import { randomBytes } from "node:crypto";
+import { TextEncoder } from "node:util";
 import argon2 from "argon2";
-import type { UserCreateInput } from "../../generated/prisma/models.js";
+import * as jose from "jose";
 import { emailRegex } from "../helpers.js";
 import { prisma } from "../lib/prisma.js";
+import type { LoginCredentials, UserCreateData } from "../types/auth.types.js";
 
-export const createUser = async (data: UserCreateInput) => {
+export const createUser = async (data: UserCreateData) => {
+  const passwordPepper = process.env.PASSWORD_PEPPER;
+
+  if (!passwordPepper) {
+    throw new Error("Missing 'PASSWORD_PEPPER' environment variable.");
+  }
+
   if (!data.firstName) {
     throw new Error("Missing 'firstName' field.");
   }
@@ -34,12 +43,6 @@ export const createUser = async (data: UserCreateInput) => {
     throw new Error("Missing 'password' field.");
   }
 
-  const passwordPepper = process.env.PASSWORD_PEPPER;
-
-  if (!passwordPepper) {
-    throw new Error("Missing 'PASSWORD_PEPPER' environment variable.");
-  }
-
   const hash = await argon2.hash(data.password, {
     type: argon2.argon2id,
     secret: Buffer.from(passwordPepper),
@@ -50,32 +53,89 @@ export const createUser = async (data: UserCreateInput) => {
       firstName: data.firstName,
       lastName: data.lastName,
       email: data.email,
-      password: hash,
+      hashedPassword: hash,
     },
   });
 
   return createdUser;
 };
 
-// export const testingArgon2Hash = async () => {
-//   const password = "johnnydoe";
+export const loginUser = async (data: LoginCredentials) => {
+  const passwordPepper = process.env.PASSWORD_PEPPER;
+  const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+  const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
-//   const hash = await argon2.hash(password, {
-//     type: argon2.argon2id,
-//     secret: Buffer.from(peppers),
-//   });
+  if (!passwordPepper) {
+    throw new Error("Missing 'PASSWORD_PEPPER' environment variable.");
+  }
 
-//   return hash;
-// };
+  if (!accessTokenSecret) {
+    throw new Error("Missing 'ACCESS_TOKEN_SECRET' environment variable.");
+  }
 
-// export const testingArgon2Verify = async () => {
-//   const t = await argon2.verify(hashedPassword, "johnnydoe", {
-//     secret: Buffer.from(peppered),
-//   });
+  if (!refreshTokenSecret) {
+    throw new Error("Missing 'REFRESH_TOKEN_SECRET' environment variable.");
+  }
 
-//   console.log("passwordToHash:", "johnnydoe");
-//   console.log("hashedPassword:", hashedPassword);
-//   console.log("peppers:", peppered);
+  if (!data.email) {
+    throw new Error("Missing 'email' field.");
+  }
 
-//   return t;
-// };
+  if (!data.password) {
+    throw new Error("Missing 'password' field.");
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (!user) {
+    throw new Error("Invalid login credentials.");
+  }
+
+  const matchingPassword = await argon2.verify(
+    user.hashedPassword,
+    data.password,
+    {
+      secret: Buffer.from(passwordPepper),
+    },
+  );
+
+  if (!matchingPassword) {
+    throw new Error("Invalid login credentials.");
+  }
+
+  const refreshToken = randomBytes(64).toString("base64");
+  const hashedRefreshToken = await argon2.hash(refreshToken, {
+    type: argon2.argon2id,
+    secret: Buffer.from(refreshTokenSecret),
+  });
+
+  // Thirty days from now
+  const expiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
+
+  await prisma.refreshToken.create({
+    data: {
+      hashedToken: hashedRefreshToken,
+      expiresAt: expiresAt,
+      userId: user.id,
+    },
+  });
+
+  const secret = new TextEncoder().encode(accessTokenSecret);
+  const jwt = await new jose.SignJWT({ sub: user.id })
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("10min")
+    .sign(secret);
+
+  return {
+    jwt,
+    refreshToken: {
+      token: refreshToken,
+      expiresAt,
+    },
+  };
+};
