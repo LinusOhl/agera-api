f8b21e153af79f6bdfe0413e2bb3d7ce5e90fbea
Linuss <tade3d@gmail.com>
2026-01-08 11:02:18 +0100

    Created simple isUser middleware.

---
+++ src/middlewares/isUser.ts
@@ -0,0 +1,17 @@
+import { createMiddleware } from "hono/factory";
+
+export const isUser = createMiddleware(async (c, next) => {
+  const authHeader = c.req.header("Authorization");
+
+  if (!authHeader) {
+    throw new Error("Missing authorization header.");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    throw new Error("Missing JWT token.");
+  }
+
+  next();
+});
