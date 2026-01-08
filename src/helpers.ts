e08d73d5ca1bae22c1ed4be89fdfc0c92b5f9837
Linuss <tade3d@gmail.com>
2026-01-08 14:18:55 +0100

    Reworked isUser middleware.
    - Removed helper function.
    - isUser middleware returns the userId from the JWT token.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,20 +1,3 @@
-import * as jose from "jose";
-
 // Note: https://emailregex.com/
 export const emailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-export const getUserIdFromToken = async (token: string) => {
-  const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
-
-  if (!accessTokenSecret) {
-    throw new Error("Missing 'ACCESS_TOKEN_SECRET' environment variable.");
-  }
-
-  const secret = new TextEncoder().encode(accessTokenSecret);
-  const { payload } = await jose.jwtVerify(token, secret);
-
-  const userId = payload.sub;
-
-  return userId;
-};
--- src/middlewares/isUser.ts
+++ src/middlewares/isUser.ts
@@ -1,6 +1,13 @@
 import { createMiddleware } from "hono/factory";
+import * as jose from "jose";
 
 export const isUser = createMiddleware(async (c, next) => {
+  const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+
+  if (!accessTokenSecret) {
+    throw new Error("Missing 'ACCESS_TOKEN_SECRET' environment variable.");
+  }
+
   const authHeader = c.req.header("Authorization");
 
   if (!authHeader) {
@@ -12,6 +19,17 @@ export const isUser = createMiddleware(async (c, next) => {
   if (!token) {
     throw new Error("Missing JWT token.");
   }
+
+  const secret = new TextEncoder().encode(accessTokenSecret);
+  const { payload } = await jose.jwtVerify(token, secret);
+
+  const userId = payload.sub;
+
+  if (!userId) {
+    throw new Error("Unauthorized.");
+  }
+
+  c.set("userId", userId);
 
   await next();
 });
