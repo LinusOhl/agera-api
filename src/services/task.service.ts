9e62d1d6ec62ef0d5d116e63e85186a036241742
Linuss <tade3d@gmail.com>
2026-01-10 09:24:33 +0100

    Added minor error handling.

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,3 +1,4 @@
+import { PrismaClientValidationError } from "@prisma/client/runtime/client";
 import { HTTPException } from "hono/http-exception";
 import type {
   TaskCreateInput,
@@ -10,61 +11,91 @@ export const createTask = async (
   userId: string,
   data: TaskCreateInput,
 ): Promise<TaskModel> => {
-  const task = await prisma.task.create({
-    data: {
-      title: data.title,
-      description: data.description,
-      userId: userId,
-    },
-  });
+  try {
+    const task = await prisma.task.create({
+      data: {
+        title: data.title,
+        description: data.description,
+        status: data.status,
+        userId: userId,
+      },
+    });
 
-  return task;
-};
+    return task;
+  } catch (error) {
+    if (error instanceof PrismaClientValidationError) {
+      throw new HTTPException(400, {
+        message: "Missing field or incorrect field type.",
+      });
+    }
 
-export const getTasksByUserId = async (userId: string) => {
-  const tasks = await prisma.task.findMany({
-    where: {
-      userId,
-    },
-  });
-
-  return tasks;
-};
-
-export const getTaskById = async (userId: string, taskId: string) => {
-  const task = await prisma.task.findUnique({
-    where: {
-      id: taskId,
-      userId,
-    },
-  });
-
-  if (!task) {
-    throw new HTTPException(404, { message: "Task not found." });
+    throw new HTTPException(400, { cause: error });
   }
+};
 
-  return task;
+export const getTasksByUserId = async (
+  userId: string,
+): Promise<TaskModel[]> => {
+  try {
+    const tasks = await prisma.task.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    return tasks;
+  } catch (error) {
+    throw new HTTPException(400, { cause: error });
+  }
+};
+
+export const getTaskById = async (
+  userId: string,
+  taskId: string,
+): Promise<TaskModel | null> => {
+  try {
+    const task = await prisma.task.findUnique({
+      where: {
+        id: taskId,
+        userId,
+      },
+    });
+
+    return task;
+  } catch (error) {
+    throw new HTTPException(400, { cause: error });
+  }
 };
 
 export const updateTaskById = async (
   userId: string,
   taskId: string,
   data: TaskUpdateInput,
-) => {
+): Promise<TaskModel> => {
   const now = new Date(Date.now());
 
-  const task = await prisma.task.update({
-    where: {
-      id: taskId,
-      userId,
-    },
-    data: {
-      title: data.title,
-      description: data.description,
-      status: data.status,
-      updatedAt: now,
-    },
-  });
+  try {
+    const task = await prisma.task.update({
+      where: {
+        id: taskId,
+        userId,
+      },
+      data: {
+        title: data.title,
+        description: data.description,
+        status: data.status,
+        updatedAt: now,
+      },
+    });
 
-  return task;
+    return task;
+  } catch (error) {
+    if (error instanceof PrismaClientValidationError) {
+      throw new HTTPException(400, {
+        message: "Missing field or incorrect field type.",
+      });
+    }
+
+    throw new HTTPException(400, { cause: error });
+  }
 };
