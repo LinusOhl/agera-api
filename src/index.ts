c3b21718ebba147c9ccd00e92007edc06c6f7ebf
Linuss <tade3d@gmail.com>
2026-01-12 16:20:45 +0100

    Changed port + added cors handling.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
 import authRoutes from "./routes/auth.js";
 import tasksRoutes from "./routes/tasks.js";
@@ -11,6 +12,14 @@ export type Variables = {
 const app = new Hono().basePath("/api");
 
 app.use(prettyJSON());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 app.notFound((c) =>
   c.json(
@@ -35,7 +44,7 @@ app.route("/tasks", tasksRoutes);
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: 3001,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
